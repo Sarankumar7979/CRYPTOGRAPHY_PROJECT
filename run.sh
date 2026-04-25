5e3e9a700df600d213a54c6e0efd1cf90af25f5e
voidbreaker <v01dbr3ak3r@gmail.com>
2026-04-25 23:22:46 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-gcc main.c SHA_256.c AES.c DES.c RSA.c -o crypto #or use clang
+gcc main.c SHA_256.c -o crypto #or use clang
 
 if [ $? -ne 0 ]; then
     echo "Compilation failed"
     exit 1
 fi
 
-echo "Compilation successful. Running tests..."
+echo "Compilation successful."
+echo "Running tests..."
 
 # proper mapping (index must match C switch cases)
-algos=("SHA-256" "AES" "DES" "RSA")
+algos=("SHA256" "AES" "DES" "RSA")
 
-for i in "${!algos[@]}"
+echo " "
+for algo in "${algos[@]}"
 do
-    echo "Running ${algos[$i]}..."
-    ./crypto "$i" test.txt
+    echo "Running $algo algorithm ..."
+    ./crypto "$algo" test.txt
+    echo " "
 done
